c743655f74df8810c5287611fa1bae544a8827bb
Stanley  Gonleh <77513627+sgonleh@users.noreply.github.com>
2025-05-06 07:37:20 -0400

    Add files via upload

---
+++ #Integers and floats.py
@@ -0,0 +1,15 @@
+#Integers and floats
+age = 25
+height = 5.9
+
+#Strings
+name = "Alice"
+greeting = "Hello, " + name 
+#list and tuples
+numbers = [1, 2, 3, 4,]
+names = ["Alice", "Jack",]
+coordinates = (10, 20)
+#dictionaries
+person = {"name": "Alice", "age": 25}
+#booleans
+is_student = True
