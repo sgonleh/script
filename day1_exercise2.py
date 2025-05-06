4c30a25206d8bcd5b6a54505f01759714e119182
Stanley  Gonleh <77513627+sgonleh@users.noreply.github.com>
2025-05-06 07:39:44 -0400

    Add files via upload

---
+++ day1_exercise2.py
@@ -0,0 +1,18 @@
+# Define variables of different data types
+interger_var = 10
+float_var = 3.14
+string_var = "AI"
+list_var = [1, 2, 3]
+tuple_var = (4, 5, 6)
+dict_var = {"name": "Alice", "role": "Engineer"}
+bool_var = True
+
+# print and manipulate the variables
+print("Integer:", interger_var)
+print("Float:", float_var)
+print("String:", string_var + " Bootcamp")
+list_var.append(4)
+print("List:", list_var)
+print("Tuple:", tuple_var)
+print("Dictionary:", dict_var ["role"])
+print("Boolean:", bool_var)
